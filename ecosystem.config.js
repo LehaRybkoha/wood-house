794afbb0a755788c00a36aeb27703d76a87099c5
LehaRybkoha <vintuvintu34@gmail.com>
2024-08-01 05:14:33 +0300

    fix

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,20 +1,23 @@
 module.exports = {
-  apps : [{
-    script: 'npm run start',
-  }],
+  apps: [
+    {
+      script: "npm run start",
+    },
+  ],
 
-  deploy : {
-    production : {
-	   // key: 'key.pem',
-	    user : 'root',
-      host : '194.58.126.86',
-      ref  : 'origin/main',
-      repo : 'git@github.com:LehaRybkoha/wood-house.git',
-      path : '/home/ubuntu',
-      'pre-deploy-local': '',
-      'post-deploy': 'source ~/.nvm/nvm.sh && cd front && npm install && npm run build && pm2 reload ecosystem.config.js --env production',
-      'pre-setup': '',
-	'ssh_options': 'ForwardAgent=yes'
-    }
-  }
+  deploy: {
+    production: {
+      // key: 'key.pem',
+      user: "root",
+      host: "194.58.126.86",
+      ref: "origin/main",
+      repo: "git@github.com:LehaRybkoha/wood-house.git",
+      path: "/home/ubuntu",
+      "pre-deploy-local": "",
+      "post-deploy":
+        "source ~/.nvm/nvm.sh && cd back && npm install && npm run start && pm2 reload ecosystem.config.js --env production",
+      "pre-setup": "",
+      ssh_options: "ForwardAgent=yes",
+    },
+  },
 };
