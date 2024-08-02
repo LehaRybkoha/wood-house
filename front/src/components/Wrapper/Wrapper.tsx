8c42520435fc4ce53e3c58f4da97306ad3820e57
LehaRybkoha <vintuvintu34@gmail.com>
2024-08-02 03:15:33 +0300

    fix

--- front/src/components/Wrapper/Wrapper.tsx
+++ front/src/components/Wrapper/Wrapper.tsx
@@ -21,7 +21,7 @@ export type AppProps = {
 export const Wrapper: FC<AppProps> = ({children}) => {
     const [theme] = React.useState<Theme>(DEFAULT_THEME);
 
-    const [isMenuOpen, setMenuOpen] = useState(!(window.innerWidth < 992));
+    const [isMenuOpen, setMenuOpen] = useState(false);
 
     const toggleMenu = () => {
         setMenuOpen(!isMenuOpen);
